ab81eb3247e06b956c3d85b07c0f062686bf7c33
Stanislav Cingel <stanislav@cingel.sk>
2024-11-13 00:45:20 +0100

    Implemented data ingestion

--- obec-api/main.py
+++ obec-api/main.py
@@ -1,1 +1,42 @@
-print("Hello world")
+from fastapi import FastAPI
+import requests
+import sqlite3
+import csv
+
+DATA_URL = "https://www.datakhk.cz/api/download/v1/items/3ec5df3a89d7470ea40330baadca74f4/csv?layers=0"
+
+app = FastAPI()
+
+@app.get("/obce")
+def get_obce(district: str, zip_code: str):
+    conn = sqlite3.connect("data.sqlite")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM data WHERE district = ? AND zip_code = ?", (district, zip_code))
+    data = cursor.fetchall()
+
+    return data
+
+def import_data():
+    print("Importing data...")
+    # Download data from DATA_URL and save to sqlite database
+    response = requests.get(DATA_URL)
+
+    # Create sqlite database
+    conn = sqlite3.connect("data.sqlite")
+    cursor = conn.cursor()
+
+    # Create table
+    cursor.execute("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, name TEXT, district TEXT, zip_code TEXT, web TEXT)")
+
+    # Insert data
+    csv_file = response.text.splitlines()
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        print(row)
+        cursor.execute("INSERT INTO data (name, district, zip_code, web) VALUES (?, ?, ?, ?)", (row["Název obce"], row["Název okresu"], row["PSČ"], row["Webové stránky"]))
+    conn.commit()
+    
+    print("Data imported.")
+
+import_data()
